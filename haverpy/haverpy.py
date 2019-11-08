a2f5d776763ea81a44e297e67db06efc13d8a627
Santiago Sordo-Palacios <ssordopalacios@gmail.com>
2019-11-08 13:33:30 -0600

    WIP: Minor syntax changes, add diff and trim

--- haverpy/haverpy.py
+++ haverpy/haverpy.py
@@ -1,4 +1,4 @@
-# haverpy.py
+# py
 
 # Load needed packages
 import Haver
@@ -22,13 +22,13 @@ def check_db(db):
 
 def fetch(series):
     # Set the Haver Path
-    haverpy.set_path()
+    set_path()
 
     # Clean the series names
     split_series = series.split('@')
     code = split_series[0]
     db = split_series[1]
-    haverpy.check_db(db)
+    check_db(db)
 
     # Get the data
     df = Haver.data(code, db)
@@ -37,18 +37,18 @@ def fetch(series):
     freq = 'D'
     how = 'E'
     df.index = df.index.to_timestamp(freq, how)
-    return df
+    return(df)
 
 
 def info(series):
     # Set the Haver Path
-    haverpy.set_path()
+    set_path()
 
     # Clean the series names
     split_series = series.split('@')
     code = split_series[0]
     db = split_series[1]
-    haverpy.check_db(db)
+    check_db(db)
 
     # Get the metadata and conver to dictionary
     info = Haver.metadata(code, db)
@@ -56,22 +56,36 @@ def info(series):
     return info_as_dict
 
 
-def merge(df1, df2, how='outer'):
-    # Check the inputs are the same frequency
-    freq1 = pd.infer_freq(df1.index)
-    freq2 = pd.infer_freq(df2.index)
-    assert(freq1 == freq2)
+def merge(df1, df2, how='outer', check_freq=True):
+    if check_freq:
+        # Check the inputs have the same frequency
+        freq1 = pd.infer_freq(df1.index)
+        freq2 = pd.infer_freq(df2.index)
+        assert(freq1 == freq2)
 
     # Perform an a join with 'how'
     df = df1.join(df2, how=how)
-    return df
+    return(df)
+
+
+def trim(df, start_date='', end_date=''):
+
+    trimmed = df
+
+    if start_date:
+        trimmed = trimmed.truncate(before=start_date)
+
+    if end_date:
+        trimmed = trimmed.truncate(after=end_date)
+
+    return(trimmed)
 
 
 def multiseries_function(df1, df2, type_function):
     # Check that at least one input is a dataframe
     assert(isinstance(df1, pd.DataFrame) or isinstance(df2, pd.DataFrame))
 
-    # Create a dataframe if adding just one value
+    # Create a dataframe out of a single int
     if isinstance(df1, int):
         nrows = len(df2.index)
         value = np.repeat(df1, nrows)
@@ -82,12 +96,12 @@ def multiseries_function(df1, df2, type_function):
         value = np.repeat(df2, nrows)
         df2 = pd.DataFrame(value, index=df1.index, columns=['value2'])
 
-    # Rename the variables coming in
+    # Rename the variables coming in to allow for merge
     df1.columns = ['value1']
     df2.columns = ['value2']
 
     # Merge the two series together
-    df = haverpy.merge(df1, df2)
+    df = merge(df1, df2)
 
     # Perform the specified operation type
     if type_function == 'addition':
@@ -133,13 +147,46 @@ def division(df1, df2):
 
 def lag(df, periods=1):
     # Create a lag
-    assert(np.sign(periods) == 1)
-    df = df.shift(periods)
-    return(df)
+    lagged = df.shift(periods)
+    return(lagged)
 
 
-def lead(df, periods=-1):
+def lead(df, periods=1):
     # Create a lead
-    assert(np.sign(periods) == -1)
-    df = df.shift(periods)
-    return(df)
+    leaded = lag(df, periods=-periods)
+    return(leaded)
+
+
+def diff(df, periods=1):
+    # Calculate the change
+    df1 = df
+    df2 = lag(df1, periods)
+    diffed = subtraction(df1, df2)
+    return(diffed)
+
+
+def diff_bps(df, periods=1):
+    # Calculate the change in basis points
+    diffed = diff(df, periods)
+    diffed_bps = multiplication(diffed, 100)
+    return(diffed_bps)
+
+
+def diff_pct(df, periods=1):
+    # Calculate the change as a percent
+    df1 = df
+    df2 = lag(df1, periods)
+    # diffed_pct = 100 * (df1 / df2 - 1)
+    divided = division(df1, df2)
+    subtracted = subtraction(divided, 1)
+    diffed_pct = multiplication(100, subtracted)
+    return(diffed_pct)
+
+
+def movsum(df, window):
+
+
+
+
+
+
